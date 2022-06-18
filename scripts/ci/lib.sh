f384277e9701d2e6c3d21c127e8cdb6db1e72538
Gavin Jefferies <gavin@stackrox.com>
2022-06-18 00:00:13 +0000

    remove check-scanner-and-collector from CCI

--- scripts/ci/lib.sh
+++ scripts/ci/lib.sh
@@ -341,40 +341,27 @@ check_docs() {
 }
 
 check_scanner_and_collector() {
-    info "Check on release builds that COLLECTOR_VERSION and SCANNER_VERSION are release"
+    info "Check on builds that COLLECTOR_VERSION and SCANNER_VERSION are release versions"
 
     if [[ "$#" -ne 1 ]]; then
-        die "missing arg. usage: check_scanner_and_collector <fail-on-rc>"
-    fi
-
-    local fail_on_rc="$1"
-    local main_release_like=0
-    local main_rc=0
-    local main_tag
-    main_tag="$(make --quiet tag)"
-    if is_release_version "$main_tag"; then
-        main_release_like=1
-    fi
-    if is_RC_version "$main_tag"; then
-        main_release_like=1
-        main_rc=1
+        die "missing arg. usage: check_scanner_and_collector"
     fi
 
     local release_mismatch=0
-    if ! is_release_version "$(make --quiet collector-tag)" && [[ "$main_release_like" == "1" ]]; then
+    if ! is_release_version "$(make --quiet collector-tag)"; then
         echo >&2 "Collector tag does not look like a release tag. Please update COLLECTOR_VERSION file before releasing."
         release_mismatch=1
     fi
-    if ! is_release_version "$(make --quiet scanner-tag)" && [[ "$main_release_like" == "1" ]]; then
+    if ! is_release_version "$(make --quiet scanner-tag)"; then
         echo >&2 "Scanner tag does not look like a release tag. Please update SCANNER_VERSION file before releasing."
         release_mismatch=1
     fi
 
-    if [[ "$release_mismatch" == "1" && ( "$main_rc" == "0" || "$fail_on_rc" == "true" ) ]]; then
-        # Note that the script avoids doing early exits in order for the most of its logic to be executed drung
-        # regular pipeline runs so that it does not get rusty by the time of the release.
-        exit 1
+    if [[ "$release_mismatch" == "1" ]]; then
+        return 1
     fi
+
+    info "The scanner and collector versions are release versions"
 }
 
 mark_collector_release() {
